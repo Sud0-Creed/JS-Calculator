b79a4f1f5b33543bcc5e43ebeadbfa109f01854e
Sud0 Creed <sudocreed@gmail.com>
2023-05-23 18:58:22 +1000

    Added module type in script tag, html element

--- app.js
+++ app.js
@@ -1,47 +1,38 @@
 //Import my modules
-import { equal, clear, del } from "./calculations"
-
+import { equal, clear, del } from "./calculations.js";
 
 //* Target DOM elements
-const display = document.querySelector('.input')
-const pad = document.querySelector('.pad-grid')
+const display = document.querySelector(".input");
+const pad = document.querySelector(".pad-grid");
 
-console.log(display)
-console.log(pad.innerHTML)
+console.log(display);
+console.log(pad.innerHTML);
 
 //* Added event listen to the parent element.
 //* Used event bubbling on parent element.
-pad.addEventListener('click', function (e) {
-
-
+pad.addEventListener("click", function (e) {
+  
     if (e.target.className == "pad") {
+   
+    //Get the value of the button clicked
+    const div = e.target.innerHTML;
 
-        //Get the value of the button clicked
-        const div = e.target.innerHTML;
+    //Append the value clicked to the display
+    display.innerHTML += div;
+  }
+  
+  else if (e.target.className == "equal") {
+    //Get all the inputs
+    const output = display.innerHTML;
 
-        //Append the value clicked to the display
-        display.innerHTML += div
+    //Pass output into function
+    let answer = equal(output);
 
+    display.textContent = answer;
+  } 
+  
+  else if (e.target.className == "backspace") {
+    del();
+  }
 
-    } else if (e.target.className == 'equal') {
-
-        //Get all the inputs
-        const output = display.innerHTML
-
-        //Pass output into function
-        let answer = equal(output)
-
-        display.textContent = answer
-
-    }
-
-    else if (e.target.className == 'clear'){
-
-        clear()
-
-    }
-
-
-})
-
-
+});
