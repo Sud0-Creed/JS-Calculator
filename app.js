3265de77efa7249efd7c86e5e309db70b126966b
Sud0 Creed <sudocreed@gmail.com>
2023-05-23 19:39:32 +1000

    Event bubling. Added an event listener to the parent element. All pads are working. Except other functional pads.

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
-
 //* Target DOM elements
 
 const display = document.querySelector('.input')
-const pad = document.querySelector('.pad-grid .one')
-
+const pad = document.querySelector('.pad-grid')
 
 console.log(display)
 console.log(pad.innerHTML)
 
+//* Added event listen to the parent element.
+//* Used event bubbling on parent element.
+pad.addEventListener('click', function (e) {
 
-pad.addEventListener('click', function(){
+    if (e.target.className == "pad") {
 
-//Get the value of the button clicked
-let value = pad.innerHTML
+        //Get the value of the button clicked
+        const div = e.target.innerHTML;
 
-//Append the value clicked to the display
-display.innerHTML += value
-
+        console.log(div)
+        //Append the value clicked to the display
+        display.innerHTML += div
+    }
 
 })
 
