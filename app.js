177dd6a54043c373be2dde47748dfea91f0cb2ec
Sud0 Creed <sudocreed@gmail.com>
2023-05-23 19:32:45 +1000

    Testing: Added event handler to one button. And change display. Function works, change html in UI branch

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 
 //* Target DOM elements
 
+const display = document.querySelector('.input')
+const pad = document.querySelector('.pad-grid .one')
 
 
+console.log(display)
+console.log(pad.innerHTML)
+
+
+pad.addEventListener('click', function(){
+
+//Get the value of the button clicked
+let value = pad.innerHTML
+
+//Append the value clicked to the display
+display.innerHTML += value
+
+
+})
 
 
 
