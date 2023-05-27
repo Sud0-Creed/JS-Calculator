d64ca5ca18418c5e8835c3a9b1ed03e682686e53
Sud0 Creed <sudocreed@gmail.com>
2023-05-28 04:53:25 +1000

    Set development mode in webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
+    mode:'development',
 
     //* Set entry point of webpack bundling
     entry: "./src/app.js",
