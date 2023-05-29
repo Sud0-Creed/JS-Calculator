2e5db7de88ea5a94c5616604b3876f562bdd341c
Sud0 Creed <sudocreed@gmail.com>
2023-05-29 19:14:54 +1000

    New Images loader Installed.

--- webpack.config.js
+++ webpack.config.js
@@ -41,7 +41,32 @@ module.exports = {
             
             {
                 test: /\.(png|jpeg|jpg|gif|svg)$/i, // Match image files
-                type: 'asset/resource'
+                type: 'asset/resource',
+                use:['file-loader',
+                {
+                  loader: 'image-webpack-loader',
+                  options: {
+                    mozjpeg: {
+                      progressive: true,
+                    },
+                    // optipng.enabled: false will disable optipng
+                    optipng: {
+                      enabled: false,
+                    },
+                    pngquant: {
+                      quality: [0.65, 0.90],
+                      speed: 4
+                    },
+                    gifsicle: {
+                      interlaced: false,
+                    },
+                    // the webp option will enable WEBP
+                    webp: {
+                      quality: 75
+                    }
+                  }
+                  
+                },]
             }, 
             
             {
